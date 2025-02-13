e06f7b61063e581e8791693d8dd3ee6f9afd1494
Jacob Stone <jumboshrimp9554@gmail.com>
2025-02-12 21:14:33 -0500

    added some text and an image. working on aliging the button to where I want it. will add button functionality ASAP.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("clickMe").addEventListener("click", function() {
     //gsap code will go here :)
 
+    //OPEN THE ENVELOPE
     gsap.to("polygon:nth-of-type(1)", {
         scaleY: -1,
         transformOrigin: "center",
@@ -9,38 +10,68 @@ document.getElementById("clickMe").addEventListener("click", function() {
         delay:0
     });
 
-     gsap.to("#heart", {
-        scaleY: -1,
-        transformOrigin: "center",
-        y: -200,
-        duration: 1,
-        delay:0
-     });
-
-     gsap.to("rect:nth-of-type(2)", {
+    gsap.to("#heart", {
+       scaleY: -1,
+       transformOrigin: "center",
+       y: -200,
+       duration: 1,
+       delay:0
+      });
+      
+     //MOVE LETTER UP
+     gsap.to("rect:nth-of-type(2), rect:nth-of-type(3)", {
         y: -300,
         duration: 1,
         opacity: 1,
         delay: 1
      });
 
+     gsap.to("text", {
+      y: -300,   
+      opacity: 1,
+      duration: 1,
+      delay: 1
+      });
 
-     gsap.to("polygon:nth-of-type(1), polygon:nth-of-type(2), polygon:nth-of-type(3), polygon:nth-of-type(4), rect:nth-of-type(1), rect:nth-of-type(3)", {
+      gsap.to("image", {
+         y: -300,   
+         opacity: 1,
+         duration: 1,
+         delay: 1
+         });
+  
+     // MOVE ENVELOPE DOWN
+     gsap.to("polygon:nth-of-type(1), polygon:nth-of-type(2), polygon:nth-of-type(3), polygon:nth-of-type(4), rect:nth-of-type(1), rect:nth-of-type(4)", {
         y:1200,
         duration: 1,
         delay: 3
      });
 
-     gsap.to("rect:nth-of-type(2)", {
-        duration: 0.5,
+
+     // MOVE LETTER DOWN
+   //   gsap.to("rect:nth-of-type(2), rect:nth-of-type(3)", {
+   //      duration: 0.5,
+   //      delay: 4
+   //   });
+
+     gsap.to("rect:nth-of-type(2), rect:nth-of-type(3)", {
+        y: -175,
+        duration: 1,
         delay: 4
      });
 
-     gsap.to("rect:nth-of-type(2)", {
-        y: -175,
-        duration: 1,
-        delay: 5
-     });
+     gsap.to("text", {
+      y: -175,   
+      duration: 1,
+      delay: 4
+      });
+
+      gsap.to("image", {
+         y: -175,   
+         duration: 1,
+         delay: 4
+         });
+
  
 });
 
@@ -71,4 +102,9 @@ document.getElementById("reset").addEventListener("click", function() {
         y: 525,
         duration:0.5
     });
+
+    gsap.to("text", { 
+      y: 0,  // Reset text to original position
+      duration: 0.5
+   });
 });
