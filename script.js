e9122cc5db5bfc84f72072d4672a6bf4757431a6
Jacob Stone <jumboshrimp9554@gmail.com>
2025-02-13 13:58:46 -0500

    added some heart-shaped confetti to the yes button with tsParticles

--- script.js
+++ script.js
@@ -166,6 +166,35 @@ document.getElementById("yes").addEventListener("mouseleave", () => {
 
 //yes click
 document.getElementById("yes").addEventListener("click", () => {
+
+  const defaults = {
+    spread: 360,
+    ticks: 100,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["heart"],
+    colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+  };
+  
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+  });
+  
+  confetti({
+    ...defaults,
+    particleCount: 25,
+    scalar: 3,
+  });
+  
+  confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 4,
+  });
+  
    gsap.to("line:nth-of-type(1), line:nth-of-type(2)", {
       opacity: 1,
       duration: 1
